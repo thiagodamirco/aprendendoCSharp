91e76be809b9fa18cfc64c3d7a877117585acb4c
thiagodamirco <thiago_damirco@yahoo.com.br>
2025-10-18 19:02:29 -0300

    Delete HelloWorld directory
    pasta extra deletada

--- HelloWorld/Cor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HelloWorld;
-public enum Cor //enum é um tipo de valor que consiste em um conjunto nomeado de constantes chamadas de enumeradores.
-{
-    Vermelho,
-    Azul,
-    Verde,
-    Preto,
-    Branco
-}
--- HelloWorld/StaticMatematica.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace HelloWorld;
-public static class StaticMatematica // classe estática não pode ser instanciada e o valor dela é compartilhado em toda a aplicação
-{
-    public static int Somar(int valor1, int valor2) => valor1 + valor2; // método estático que retorna a soma de dois valores inteiros
-
-    public static int Subtrair(int valor1, int valor2) => valor1 - valor2; // todas as funções dentro de uma classe estática obrigatoriamente precisam ser estáticas
-}
